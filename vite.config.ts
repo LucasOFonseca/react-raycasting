6418ffe2bab2f7d80a5a2422baffa6f46106ad58
Lucas Fonseca <lucasfonsecaeditor@gmail.com>
2024-12-03 10:49:38 -0300

    fix: build issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import react from '@vitejs/plugin-react';
-import path from 'path';
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': resolve(__dirname, 'src'),
     },
   },
 });
